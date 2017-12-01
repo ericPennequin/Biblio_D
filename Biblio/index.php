8bb9d96c4d9bd0ebe426718f092c5208e6d8b320
Romain Goguet <rom.goguet@laposte.net>
2017-12-01 12:55:45 +0100

    Add files via upload

--- Biblio/index.php
+++ Biblio/index.php
@@ -8,22 +8,98 @@
 $title ="accueil";
 include "header.php";
 ?>
-<div class="container page">
-    <div class="row">
+    <div class="container page">
 
-        <small><?php echo $title; ?></small>
+        <div class="col-md lastadd">
+            <div class="row">
+                <span>Derniers ajouts</span>
+            </div>
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th class="col2">
+                            <img src="img/spongebob.jpg" alt="" style="border-style: double">
+                        </th>
+                        <td class="col-2">
+                            <div class="row">
+                                Titre : Livre
+                            </div>
+                            <div class="row">
+                                Tags : Lorem, Lorem, Lorem
+                            </div>
+                            <div class="row">
+                                Auteur: Lorem
+                            </div>
+                            <div class="row">
+                                Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut vulputate lorem. Vestibulum sollicitudin eros rutrum nunc ultricies, eget venenatis nisl imperdiet. Aenean tortor orci, bibendum et ultrices nec, ornare id libero. Nullam pretium volutpat.
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th class="col2">
+                            <img src="img/spongebob.jpg" alt="" style="border-style: double">
+                        </th>
+                        <td class="col-2">
+                            <div class="row">
+                                Titre : Magazine
+                            </div>
+                            <div class="row">
+                                Tags :Lorem
+                            </div>
+                            <div class="row">
+                                Parution: 01-01-17
+                            </div>
+                            <div class="row">
+                                Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut vulputate lorem. Vestibulum sollicitudin eros rutrum nunc ultricies, eget venenatis nisl imperdiet. Aenean tortor orci, bibendum et ultrices nec, ornare id libero. Nullam pretium volutpat.
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+
+
+
+
+
+
+            <!-- <div class="row">
+            <div class="col-md-2 book">
+                <img src="img/spongebob.jpg" alt="" style="border-style: double">
+            </div>
+            <div class="col-md" style="border-style: double">
+                <div class="row">
+                    Titre :
+                </div>
+                <div class="row">
+                    Tags :
+                </div>
+                <div class="row">
+                    Auteur:
+                </div>
+                <div class="row">
+                    Description:
+                </div>
+            </div>
+        </div> -->
+
+        </div>
+
+
+
 
     </div>
 
 
 
-</div>
 
 
 
-
-
-
-<?php
+    <?php
 include "footer.php";
 ?>
