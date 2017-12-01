80abdd1b76c85f7001dd68ff1703d24d58473cc9
Romain Goguet <rom.goguet@laposte.net>
2017-12-01 12:16:45 +0100

    Add files via upload

--- Biblio/header.php
+++ Biblio/header.php
@@ -5,8 +5,7 @@
  * Date: 29/11/2017
  * Time: 15:34
  */
-include "inc/functions.php";
-
+include "inc/functions.php"
 
 
 ?>
@@ -24,51 +23,58 @@ include "inc/functions.php";
     <link rel="stylesheet" href="css/custom.css" >
 </head>
 <body>
-<div class="container main">
-    <div class="row">
-        <img src="img/logoCouleur.png">
-    </div>
+    <div class="main">
+        <div class="container">
 
-    <div class="row">
-        <h1 class="col-md-9 title">Biblio-D</h1>
-        <small class="col-md-3 title-small">bibliothèque du cefim</small>
-    </div>
-
-    <div class="row justify-content-center">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Biblio-D</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="index.php" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="catalogue.php">Catalogue</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-
+            <div class="container">
+                <div class="row">
+                    <div class="col-">
+                        <img src="img/logoCouleur.png">
+                    </div>
+                    <div class="col-sm">
+                        <h1>Bibliothèque des étudiants du CEFIM</h1>
+                    </div>
+                    
+                </div>
             </div>
-        </nav>
-    </div>
+
+            <div class="row justify-content-center">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Biblio-D</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="index.php" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Accueil</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="catalogue.php">Catalogue</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Disabled</a>
+                            </li>
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+
+                    </div>
+                </nav>
+            </div>
+        </div>
+    
