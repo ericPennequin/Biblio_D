5b5d5378ce30c5699394076f1ebf2615534baf44
jeromeP <jeromepageault@gmail.com>
2017-12-01 10:00:47 +0100

    index admin
    modif index admin

--- Biblio/index_admin.php
+++ Biblio/index_admin.php
@@ -1,26 +1,51 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
- * User: eric.pennequin
- * Date: 01/12/2017
- * Time: 08:58
+ * Created by PhpStorm.
+ * User: cdi
+ * Date: 29/11/2017
+ * Time: 14:16
  */
-//Vue état des lieux
-
+$title ="accueil";
 include "header.php";
-
-
 ?>
+<div class="container page">
+    <div class="container evenement">
+        <div class="row">
+            <table class="table table-striped table-bordered table-hover evenement">
+            <caption>Evênements Livres</caption>
+            <thead>
+                <tr>
+                <th class="col-md-3">Titre</th>
+                <th class="col-md-3">Auteur</th>
+                <th class="col-md-2">Emprunteur</th>
+                <th class="col-md-2">Disponible dans</th>
+                <th class="col-md-2">Retour</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>###</td>
+                <td>###</td>
+                <td>###</td>
+                <td>###</td>
+                <td class="bg-dark"><button class="btn btn-primary">Déclarer retour</button></td>
+                </tr>
+                <tr>
+                <td>###</td>
+                <td>###</td>
+                <td>###</td>
+                <td>###</td>
+                <td class="bg-dark"><button class="btn btn-primary">Déclarer retour</button></td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
 
-<script>
-
-	$.ajax{
-
-
-
-	}
+    </div>
+</div>
 
 
 
-
-</script>
+<?php
+include "footer.php";
+?>
