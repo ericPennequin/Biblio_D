34ac2df1b9a3be7bdbaae4dc81fdc76f1000e13e
Romain Goguet <rom.goguet@laposte.net>
2017-12-01 12:31:49 +0100

    Add files via upload

--- Biblio/index.php
+++ Biblio/index.php
@@ -8,22 +8,75 @@
 $title ="accueil";
 include "header.php";
 ?>
-<div class="container page">
-    <div class="row">
+    <div class="container page">
 
-        <small><?php echo $title; ?></small>
+        <div class="col-md lastadd">
+            <div class="row">
+                <span>Derniers ajouts</span>
+            </div>
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th class="col2">
+                            <img src="img/spongebob.jpg" alt="" style="border-style: double">
+                        </th>
+                        <td class="col-2">
+                            <div class="row">
+                                Titre :
+                            </div>
+                            <div class="row">
+                                Tags :
+                            </div>
+                            <div class="row">
+                                Auteur:
+                            </div>
+                            <div class="row">
+                                Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut vulputate lorem. Vestibulum sollicitudin eros rutrum nunc ultricies, eget venenatis nisl imperdiet. Aenean tortor orci, bibendum et ultrices nec, ornare id libero. Nullam pretium volutpat.
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+
+
+
+
+
+
+            <!-- <div class="row">
+            <div class="col-md-2 book">
+                <img src="img/spongebob.jpg" alt="" style="border-style: double">
+            </div>
+            <div class="col-md" style="border-style: double">
+                <div class="row">
+                    Titre :
+                </div>
+                <div class="row">
+                    Tags :
+                </div>
+                <div class="row">
+                    Auteur:
+                </div>
+                <div class="row">
+                    Description:
+                </div>
+            </div>
+        </div> -->
+
+        </div>
+
+
+
 
     </div>
 
 
 
-</div>
 
 
 
-
-
-
-<?php
+    <?php
 include "footer.php";
 ?>
